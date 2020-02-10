60eda10b7c14f71ec037adfa5df0f787445c1397
Jim Kennedy <jameskenneedy@classic.co.uk>
2020-02-10 08:35:42 +0000

    Windows fixes

--- init.el
+++ init.el
@@ -303,22 +303,22 @@
  '(column-number-mode t)
  '(custom-safe-themes
    (quote
-    ("f5512c02e0a6887e987a816918b7a684d558716262ac7ee2dd0437ab913eaec6" "40f6a7af0dfad67c0d4df2a1dd86175436d79fc69ea61614d668a635c2cd94ab" "20e359ef1818a838aff271a72f0f689f5551a27704bf1c9469a5c2657b417e6c" "bf648fd77561aae6722f3d53965a9eb29b08658ed045207fe32ffed90433eb52" "53c542b560d232436e14619d058f81434d6bbcdc42e00a4db53d2667d841702e" "146d24de1bb61ddfa64062c29b5ff57065552a7c4019bee5d869e938782dfc2a" "f0a99f53cbf7b004ba0c1760aa14fd70f2eabafe4e62a2b3cf5cabae8203113b" "60f04e478dedc16397353fb9f33f0d895ea3dab4f581307fbf0aa2f07e658a40" default)))
+    ("190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" "f5512c02e0a6887e987a816918b7a684d558716262ac7ee2dd0437ab913eaec6" "40f6a7af0dfad67c0d4df2a1dd86175436d79fc69ea61614d668a635c2cd94ab" "20e359ef1818a838aff271a72f0f689f5551a27704bf1c9469a5c2657b417e6c" "bf648fd77561aae6722f3d53965a9eb29b08658ed045207fe32ffed90433eb52" "53c542b560d232436e14619d058f81434d6bbcdc42e00a4db53d2667d841702e" "146d24de1bb61ddfa64062c29b5ff57065552a7c4019bee5d869e938782dfc2a" "f0a99f53cbf7b004ba0c1760aa14fd70f2eabafe4e62a2b3cf5cabae8203113b" "60f04e478dedc16397353fb9f33f0d895ea3dab4f581307fbf0aa2f07e658a40" default)))
  '(inhibit-startup-screen t)
- '(ispell-program-name "aspell")
  '(ispell-dictionary "british")
+ '(ispell-program-name "aspell")
  '(mouse-avoidance-mode nil nil (avoid))
  '(mouse-wheel-mode t)
  '(org-agenda-dim-blocked-tasks nil)
  '(org-agenda-todo-list-sublevels nil)
  '(org-clock-clocked-in-display (quote mode-line))
  '(org-clock-mode-line-total (quote today))
- '(org-log-done 'time)
  '(org-enforce-todo-dependencies t)
+ '(org-log-done (quote time))
  '(org-startup-indented t)
  '(package-selected-packages
    (quote
-    (csharp-mode elpy zenburn-theme undo-tree sphinx-doc realgud paredit multiple-cursors magit jedi helm-projectile git-gutter exec-path-from-shell avy)))
+    (csv-mode markdown-preview-mode csharp-mode elpy zenburn-theme undo-tree sphinx-doc realgud paredit multiple-cursors magit jedi helm-projectile git-gutter exec-path-from-shell avy)))
  '(realgud-safe-mode nil)
  '(safe-local-variable-values
    (quote
@@ -547,6 +547,9 @@
   (load cust-location))
 
 (set-time-zone-rule "GMT")
+
+(add-hook 'before-save-hook
+          'delete-trailing-whitespace)
 
 (provide '.emacs)
 ;;; .emacs ends here
